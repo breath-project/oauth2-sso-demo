e00a528cc8056543f01742b6d78e6ef972be8eac
undancer <undancer@gmail.com>
2018-08-30 16:39:46 +0800

    更新代码

--- id/src/main/java/com/undancer/id/service/ClientDetailsServiceImpl.java
+++ id/src/main/java/com/undancer/id/service/ClientDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.undancer.id.service;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
@@ -8,15 +9,26 @@ import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Properties;
 
 /**
  * Created by undancer on 2017/5/24.
  */
-public class ClientDetailsServiceImpl implements ClientDetailsService {
+public class ClientDetailsServiceImpl implements ClientDetailsService, InitializingBean {
+
+    private Properties properties;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        properties = new Properties();
+        properties.setProperty("client", "secret");
+    }
+
     public ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
         BaseClientDetails details = new BaseClientDetails();
-        details.setClientId("client");
-        details.setClientSecret("secret");
+        String clientSecret = this.properties.getProperty(clientId, clientId);
+        details.setClientId(clientId);
+        details.setClientSecret(clientSecret);
         details.setResourceIds(details.getResourceIds());
         details.setAutoApproveScopes(Arrays.asList(".*"));
         details.setScope(Arrays.asList("read", "write"));
